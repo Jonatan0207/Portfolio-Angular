bb7e4ef98426f0a3b6a5327ee02b2afe50f91faa
Jonatan Alexander Vargas <vargas2338@outlook.es>
2022-11-01 23:32:06 -0300

    ...

--- porfolio-angular/src/app/app.module.ts
+++ porfolio-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModuleTsModule } from './app-routing.module.ts/app-routing.module.ts.module'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './navbar/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
