81e3ad72485aa861db152d425130b8308e86a06b
Jonatan Alexander Vargas <vargas2338@outlook.es>
2022-11-03 00:28:07 -0300

    -.-

--- porfolio-angular/src/app/app.module.ts
+++ porfolio-angular/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModuleTsModule } from './app-routing.module.ts/app-routing.mo
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './navbar/login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    PerfilComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
