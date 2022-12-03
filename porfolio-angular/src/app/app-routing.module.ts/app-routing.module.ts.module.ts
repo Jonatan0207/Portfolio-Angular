1fe06f549d7978f3cb0086bae4b795f56ec1caa4
Jonatan0207 <vargas2338@outlook.es>
2022-12-02 21:49:32 -0300

    ...

--- porfolio-angular/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
+++ porfolio-angular/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { IntroComponent } from '../intro/intro.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'intro', component:IntroComponent},
-  {path: 'dashboard', component:DashboardComponent},
+  {path: 'intro', component: IntroComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
-
 
 
 @NgModule({
